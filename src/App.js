1d6f410942295fc6eaba317b218137a69f1f8196
Marco Riesgo Almonacid <marcoriesgo@Marcos-MacBook-Pro-2.local>
2020-05-12 20:18:55 -0700

    added the footeer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
+import './index.css';
 
 // Import Components:
 import Navbar from './components/Navbar.js'
@@ -8,7 +9,7 @@ import Dashboard from './components/Dashboard.js'
 import CreateUser from './components/CreateUser.js'
 import CategoryDashboard from './components/CategoryDashboard.js'
 import ViewUser from './components/ViewUser.js'
-
+import Footer from './components/Footer.js'
 
 class App extends Component {
   render() {
@@ -23,7 +24,9 @@ class App extends Component {
           <Route exact path='/categories/:id/users/:id' component={ViewUser} />
         </Switch>
       </Router>
-
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
   }
---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Footer extends Component {
+    render() {
+      return (
+        <div className="footer">
+          <p>Copyright © 2020 Chronos Inc. All rights reserved.</p>
+        </div>
+      );
+    }
+  }
+
+
+
+export default Footer;
